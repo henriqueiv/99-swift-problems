acc7c19a6a04a8cb460f339c66de7340f19c3ce5
Henrique Valcanaia <henriqueindalencio@gmail.com>
2018-02-27 05:39:21 -0300

    .gitignore

---
+++ swift99problems/List.swift
@@ -0,0 +1,9 @@
+//
+//  List.swift
+//  swift99problems
+//
+//  Created by Henrique Valcanaia on 21/02/18.
+//  Copyright © 2018 Henrique Valcanaia. All rights reserved.
+//
+
+import Foundation
---
+++ swift99problemsTests/ListTests.swift
@@ -0,0 +1,9 @@
+//
+//  ListTests.swift
+//  swift99problemsTests
+//
+//  Created by Henrique Valcanaia on 21/02/18.
+//  Copyright © 2018 Henrique Valcanaia. All rights reserved.
+//
+
+import Foundation
